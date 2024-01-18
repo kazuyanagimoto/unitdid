a905b632c9600d9daee4641933b286be1f578b40
kazuyanagimoto <kazuharu.yanagimoto@gmail.com>
2024-01-18 17:39:01 +0100

    Fix vignettes directory

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -4,28 +4,30 @@ files_orig <- file.path(
              recursive = TRUE, full.names = TRUE))
 
 for (file_orig in files_orig) {
-  knitr::knit(file_orig, tools::file_path_sans_ext(file_orig))
-}
+  file_path_rmd <- tools::file_path_sans_ext(file_orig)
+  knitr::knit(file_orig, output = file_path_rmd)
 
+  dir_path <- dirname(file_path_rmd)
+  source_image_path <- "figure"
+  target_image_path <- paste0(dir_path, "/figure")
 
-source_image_path <- here::here("figure")
-target_image_path <- here::here("vignettes/figure")
-
-if (dir.exists(source_image_path)) {
-  if (dir.exists(target_image_path)) {
-    unlink(target_image_path, recursive = TRUE)
+  if (dir.exists(source_image_path)) {
+    if (dir.exists(target_image_path)) {
+      unlink(target_image_path, recursive = TRUE)
+    }
+    success <- file.rename(source_image_path, target_image_path)
+  } else {
+    success <- FALSE
   }
-  success <- file.rename(source_image_path, target_image_path)
-} else {
-  success <- FALSE
+
+  if (!all(success)) {
+    stop("Image files were not successfully transferred to vignettes directory")
+  } else {
+    message("Image files were successfully transferred to vignettes directory")
+  }
 }
 
 
 
-# Clean up if successful --------------------------------------------------
 
-if (!all(success)) {
-  stop("Image files were not successfully transferred to vignettes directory")
-} else {
-  message("Image files were successfully transferred to vignettes directory")
-}
+
