b0b327ec5fcfd0de07f4a2b08835f2d787307b88
Kazuharu Yanagimoto <kazuharu.yanagimoto@gmail.com>
2024-03-08 15:33:27 +0100

    Fix bug for aggregation with NA

--- NEWS.md
+++ NEWS.md
@@ -1,25 +1,26 @@
-# unitdid 0.0.3
+## unitdid 0.0.3
 
-## New Features
+### New Features
 
 - Support for `first_stage` argument for more parametric first stage estimation
 - Support for `by` argument for aggregation
 
-## Bug Fixes
+### Bug Fixes
 
 - Bug when `data[[yname]]` includes `NA` values
+- Bug when variance estimation includes `NA` values (v0.0.3.1)
 
-# unitdid 0.0.2
+## unitdid 0.0.2
 
-## New Features
+### New Features
 
 - Support for `wname` argument for weighted estimation
 
-## Bug Fixes
+### Bug Fixes
 
 - Normalization in `summary` when `normalized = FALSE` in the model
 - Bug when `data[[tname]]` includes `NA` values
 
-# unitdid 0.0.1
+## unitdid 0.0.1
 
 - Initial GitHub submission
--- R/aggregate.R
+++ R/aggregate.R
@@ -8,18 +8,22 @@
 #' The `event` option aggregates by the group of the event timing.
 #' The `event_age` option aggregates by the group of the age at the event timing.
 #' `event_age` requires the `bname` to be provided in the model.
+#' @param na.rm Logical. If `TRUE`, remove `NA` values for the aggregation.
+#' The default is `TRUE`.
 #' @param by A character vector of variables to aggregate separately by.
 #' Default is inherited from the `unitdid` object but you can override it here.
 #' You can estimate the unit-level DiD effects separately by `by` in `unitdid`
 #' but you can also aggregate the estimates by (higher-level) `by` here.
-#' @param normalized Logical. If `TRUE`, the function will normalize the aggregated mean and variance
-#' by the mean of the imputed outcome variable. Default is inherited from the `unitdid` object.
+#' @param normalized Logical. If `TRUE`, the function will normalize
+#' the aggregated mean and variance by the mean of the imputed outcome variable.
+#' Default is inherited from the `unitdid` object.
 #'
 #' @return A `tibble` with the aggregated mean and variance of the estimated unit-level DiD effects
 #' @export
 #'
 aggregate_unitdid <- function(object,
                               agg = "full",
+                              na.rm = TRUE,
                               by = NULL,
                               normalized = NULL) {
 
@@ -70,13 +74,13 @@ aggregate_unitdid <- function(object,
   # Aggregation
   if (object$info$compute_var) {
     result <- object$aggregated |>
-      dplyr::summarize(mean = stats::weighted.mean(zz000mean, w = zz000w),
-                       var = pmax(stats::weighted.mean(zz000var, w = zz000w), 0),
+      dplyr::summarize(mean = stats::weighted.mean(zz000mean, w = zz000w, na.rm = na.rm),
+                       var = pmax(stats::weighted.mean(zz000var, w = zz000w, na.rm = na.rm), 0),
                        zz000w = sum(zz000w),
                        .by = by)
   } else {
     result <- object$aggregated |>
-      dplyr::summarize(mean = stats::weighted.mean(zz000mean, w = zz000w),
+      dplyr::summarize(mean = stats::weighted.mean(zz000mean, w = zz000w, na.rm = na.rm),
                        zz000w = sum(zz000w),
                        .by = by)
   }
